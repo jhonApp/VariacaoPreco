b7465ad49c9d8233cf20c9838f71fc4fe516ad6a
Jhon Everton <jhonevertonapp@outlook.com>
2023-12-26 14:15:17 -0300

    APIs de variação de ativos

--- VariacaoPreco.API/Models/PedidoPorCliente.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace VariacaoPreco.API.Models
-{
-    public class PedidoPorCliente
-    {
-        public int ClienteId { get; set; }
-        public string NomeCliente { get; set; }
-        public List<int> Pedidos { get; set; }
-    }
-}
---
+++ VariacaoPreco.Core/Entity/PriceInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariacaoPreco.Core.Entity
+{
+    public class PriceInfo
+    {
+        public int? Dia { get; set; }
+        public DateTime? Data { get; set; }
+        public double? Valor { get; set; }
+        public string? VariacaoDiaAnterior{ get; set; }
+        public string? VariacaoPrimeiroDia { get; set; }
+    }
+}
